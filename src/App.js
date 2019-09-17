6d2f40ac893d2abd83a552abc737f429ee1f0d87
Fillipe Oliveira <fillipe_martins@hotmail.com>
2019-09-17 17:00:10 -0300

    Created UseDelete.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from 'react'
 import useGet from './useGet'
 import usePost from './usePost'
 import axios from 'axios'
+import useDelete from './useDelete'
+
 
 const url = 'https://mymoney-nepomous.firebaseio.com/movimentacoes/2019-08.json'
 
 function App() {
   const data = useGet(url)
   const [postData, post] = usePost(url)
-
+  const [deleteData, remove] = useDelete()
+    
   const saveNew = () => {
     post({valor:10, descricao:'ola' })
+  }
+
+  const doRemove = () => {
+    remove('url')
   }
 
   return (
@@ -20,6 +27,8 @@ function App() {
       { data.loading && <p>Carregando dados...</p> }      
       <button onClick={saveNew} > Salvar </button>
       <pre>{JSON.stringify(postData)}</pre>
+      <button onClick={doRemove} > Deletar </button>
+      <pre>{JSON.stringify(deleteData)}</pre>
     </div>
   );
 }
---
+++ src/useDelete.js
@@ -0,0 +1,41 @@
+import {useEffect, useReducer} from 'react'
+import axios from 'axios'
+
+const reducer = (state, action) => {
+    if(action.type === 'REQUEST'){
+      return {
+        ...state,
+        loading: true
+      }
+    }
+    if(action.type === 'SUCCESS'){
+      return {
+        ...state,
+        loading: false,
+        data: action.data
+      }
+    }
+    //manipular meu estado
+    return state
+}
+
+const useDelete = () => {
+    const [data, dispatch] = useReducer(reducer,{
+        loading: false,
+        data: {}
+      })
+    const remove = url =>{
+        dispatch({type: 'REQUEST'})
+        axios
+            .delete(url, data)
+            .then(() =>{
+                dispatch({type: 'SUCCESS'})
+                console.log(res.data)
+        })
+      }
+    
+    return[data, remove]
+}
+
+
+export default useDelete
