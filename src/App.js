2b688de61df9c93da163a255ad575b6751066cbb
Fillipe Martins de Oliveira <fillipe_martins@hotmail.com>
2023-07-24 14:48:36 -0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,86 +1,37 @@
-import React from 'react'
-import Rest from './rest'
-import { objectMethod } from '@babel/types'
+import React from "react";
+import Rest from "./rest";
+import { objectMethod } from "@babel/types";
+import Header from "./components/Header";
+import Meses from "./Meses";
+import AdicionarMes from "./AdicionarMes";
 
-const baseURL = 'https://mymoney-nepomous.firebaseio.com/'
+const baseURL = "https://mymoney-nepomous.firebaseio.com/";
 
-const { useGet, usePost, useDelete } = Rest(baseURL)
+const { useGet, usePost, useDelete } = Rest(baseURL);
 
 function App() {
-  const data = useGet('meses/')
+  const data = useGet("meses/");
   //const data = useGet('movimentacoes/2019-08')
   //const [postData, post] = usePost('movimentacoes/2019-08')
   //const [deleteData, remove] = useDelete()
-    
+
   const saveNew = () => {
     // post({valor:10, descricao:'ola' })
-  }
+  };
 
   const doRemove = () => {
     // remove('movimentacoes/2019-08/-Lox2YojSKR4Pjeb7Lg-')
-  }
+  };
 
   return (
-    <div >
-      <div className='container'>
-        <nav className='navbar navbar-light bg-light'>
-          <div className='container'>
-            <a className='navbar-brand'>My Money</a>
-          </div>        
-        </nav>  
-        
-        <h2>Adicionar Mês</h2>
-
-        <select>
-          <option value='2019'>2019</option>
-          <option value='2020'>2020</option>
-        </select>
-        <select>
-          <option value='01'>01</option>
-          <option value='02'>02</option>
-        </select>
-        <button>Adicionar Mes</button>
-        {
-          data.loading && <span>Carregando.....</span>
-        }
-        {
-          !data.loading && (
-            <table className='table'>
-              <thead>
-                <tr>
-                  <th>Mês</th>
-                  <th>Previsao Entrada</th>
-                  <th>Entrada</th>
-                  <th>Previsao Saída</th>
-                  <th>Saída</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  //object.keys retorna todas as chaves do objeto Data
-                  Object
-                    .keys(data.data)
-                    .map(mes =>{
-                      return(
-                        <tr key={mes}>
-                          <td>{mes}</td>
-                          <td>{data.data[mes].previsao_entrada}</td>
-                          <td>{data.data[mes].entradas}</td>
-                          <td>{data.data[mes].previsao_saida}</td>
-                          <td>{data.data[mes].saidas}</td>
-                        </tr>
-                      )
-                    })
-                }              
-              </tbody>
-            </table>
-          )
-        }
-
-        {/*<pre>{JSON.stringify(data)}</pre> */}
+    <div>
+      <div className="container">
+        <Header />
+        <AdicionarMes />
+        <Meses />
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ src/Meses.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Rest from "./rest";
+
+const baseURL = "https://mymoney-nepomous.firebaseio.com/";
+
+const { useGet } = Rest(baseURL);
+
+const Meses = () => {
+  const data = useGet("meses");
+  if (data.loading) {
+    return <span>Carregando.....</span>;
+  }
+  return (
+    Object.keys(data.data).length > 0 && (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Mês</th>
+            <th>Previsao Entrada</th>
+            <th>Entrada</th>
+            <th>Previsao Saída</th>
+            <th>Saída</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(data.data).map((mes) => {
+            return (
+              <tr key={mes}>
+                <td>{mes}</td>
+                <td>{data.data[mes].previsao_entrada}</td>
+                <td>{data.data[mes].entradas}</td>
+                <td>{data.data[mes].previsao_saida}</td>
+                <td>{data.data[mes].saidas}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    )
+  );
+};
+
+export default Meses;
